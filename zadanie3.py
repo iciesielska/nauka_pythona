fe6ebdbd540a11ba08059fb5d107efce612455c0
izabellaciesielska <iciesielska94@gmail.com>
2020-12-13 12:35:17 +0100

    poprawa

--- zadanie3.py
+++ zadanie3.py
@@ -1,32 +1,47 @@
 hodowla = [
-    {
-    	'zwierze': 'kot brytyjski',
-    	'ilosc': 2,
-    	'cena': 2500,
-    	'kolor': 'niebieski',
-    	'samiec': 2,
-    	'samica': 0,
-    	'data_urodzenia': '10.11.2020'
-    },
-    {
-    	'zwierze': 'ragdoll',
-    	'ilosc': 4,
-    	'cena': 3500,
-    	'kolor': 'szylkret',
-    	'samiec': 0,
-    	'samica': 4,
-    	'data_urodzenia': '20.10.2020'
-    }, 
-    {
-    	'zwierze': 'maine coon',
-    	'ilosc': 8,
-    	'cena': 2700,
-    	'kolor': 'rudy',
-    	'samiec': 4,
-    	'samica': 4,
-    	'data_urodzenia': '17.09.2020'
-    }
+	{
+		'rasa': 'brytyjski',
+		'ilosc': 2,
+		'cena': 2500,
+		'kolor': 'niebieski',
+		'samiec': 2,
+		'samica': 0,
+		'data_urodzenia': '10.11.2020'
+	},
+	{
+		'rasa': 'ragdoll',
+		'ilosc': 4,
+		'cena': 3500,
+		'kolor': 'szylkret',
+		'samiec': 0,
+		'samica': 4,
+		'data_urodzenia': '20.10.2020'
+	}, 
+	{
+		'rasa': 'maine coon',
+		'ilosc': 8,
+		'cena': 2700,
+		'kolor': 'rudy',
+		'samiec': 4,
+		'samica': 4,
+		'data_urodzenia': '17.09.2020'
+	}
 ]
+
+def pokaz_dane_kota(rasa):
+	rasa_kota = rasa['rasa']
+	ilosc = rasa['ilosc']
+	cena = rasa['cena']
+	kolor = rasa['kolor']
+	samce = rasa['samiec']
+	samice = rasa['samica']
+	data_urodzenia = rasa['data_urodzenia']
+
+	print("RASA:{0}, ILOŚĆ:{1}, CENA:{2}, KOLOR:{3}, SAMCE:{4}, SAMICE:{5}, DATA URODZENIA:{6}".format(rasa_kota, ilosc, cena, kolor, samce, samice, data_urodzenia))
+
+for rasa in hodowla:
+	pokaz_dane_kota(rasa)
+
 
 suma = 0
 ilosc_kotow = 0
